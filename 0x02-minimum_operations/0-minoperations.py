f21b02fdf779a1d667ea79c89f1e84b0182fe361
Voxold <bilalajaha@gmail.com>
2024-07-11 13:26:07 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,24 +4,24 @@
 
 
 def minOperations(n):
-        """This function calculates the fewest number of operations
-            needed to result in exactly n H characters in a given file.
-                """
-                    if not isinstance(n, int):
-                                return 0
-                                ops_count = 0
-                                    clipboard = 0
-                                        done = 1
-                                            while done < n:
-                                                        if clipboard == 0:
-                                                                        clipboard = done
-                                                                                    done += clipboard
-                                                                                                ops_count += 2
-                                                                                                        elif n - done > 0 and (n - done) % done == 0:
-                                                                                                                        clipboard = done
-                                                                                                                                    done += clipboard
-                                                                                                                                                ops_count += 2
-                                                                                                                                                        elif clipboard > 0:
-                                                                                                                                                                        done += clipboard
-                                                                                                                                                                                    ops_count += 1
-                                                                                                                                                                                        return ops_count
+    """This function calculates the fewest number of operations
+    needed to result in exactly n H characters in a given file.
+    """
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count

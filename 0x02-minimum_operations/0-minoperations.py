55c938593b091c6c8e0fefa278fd0b05bdcb91d9
Voxold <bilalajaha@gmail.com>
2024-07-11 05:23:46 -0700

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This is the 0x02-minimum_operations task
+"""
+
+
+def minOperations(n):
+        """This function calculates the fewest number of operations
+            needed to result in exactly n H characters in a given file.
+                """
+                    if not isinstance(n, int):
+                                return 0
+                                ops_count = 0
+                                    clipboard = 0
+                                        done = 1
+                                            while done < n:
+                                                        if clipboard == 0:
+                                                                        clipboard = done
+                                                                                    done += clipboard
+                                                                                                ops_count += 2
+                                                                                                        elif n - done > 0 and (n - done) % done == 0:
+                                                                                                                        clipboard = done
+                                                                                                                                    done += clipboard
+                                                                                                                                                ops_count += 2
+                                                                                                                                                        elif clipboard > 0:
+                                                                                                                                                                        done += clipboard
+                                                                                                                                                                                    ops_count += 1
+                                                                                                                                                                                        return ops_count
